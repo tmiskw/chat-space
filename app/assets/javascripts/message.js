9348de4702643277f78b9a69e7e139889cea87b2
tmiskw <53873965+tmiskw@users.noreply.github.com>
2019-09-13 22:04:26 +0900

    correction of asynchronous communication of posting function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,30 @@
 $(function(){ 
      function buildHTML(message){
-      if ( message.image ) {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
-    }
-$('.new_message').on('submit', function(e){
+    //    var messageImage =``
+    //   if ( message.image ) {
+    //     messageImage = `<img class="lower-message__image" src="${message.image}">`
+    // 　}
+
+      var messageImage = message.image ? `<img class="lower-message__image" src="${message.image}">` : `` ;
+      var html = ` <div class= "message" "data-message-id"=${message.id}>
+                      <div class="upper-message">
+                        <div class="upper-message__user-name">
+                          ${message.user_name}
+                        </div>
+                        <div class="upper-message__date">
+                          ${message.date}
+                        </div>
+                      </div>
+                      <div class="lower-message">
+                        <div class="lower-message__content">
+                          ${message.content}
+                        </div>
+                          ${messageImage}
+                       </div> 
+                   </div>`
+       return html;
+    } 
+$('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -50,16 +35,17 @@ $('.new_message').on('submit', function(e){
       dataType: 'json',
       processData: false,
       contentType: false
-    })
+    }
      .done(function(data){
        var html = buildHTML(data);
        $('.messages').append(html);      
        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-       $('form')[0].reset();
+       $('#new_message')[0].reset();
      })
      .fail(function(){
        alert('error');
-     });
-     return false;
-   });
-});
+     })
+     .always(function(){
+       $('.submit-btn').prop('disabled', false);
+   })
+})
