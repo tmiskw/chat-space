ad5783accca8747f5e1fa47aae9be3a54016dce0
tmiskw <53873965+tmiskw@users.noreply.github.com>
2019-09-14 21:32:37 +0900

    Incremental search implementation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,28 +19,30 @@ $(function(){
                    </div>`
        return html;
     } 
-$('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    }
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.messages').append(html);      
-       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-       $('#new_message')[0].reset();
-     })
-     .fail(function(){
-       alert('error');
-     })
-     .always(function(){
-       $('.submit-btn').prop('disabled', false);
-   })
-})
+
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+       .done(function(data){
+         var html = buildHTML(data);
+         $('.messages').append(html);      
+         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+         $('#new_message')[0].reset();
+       })
+       .fail(function(){
+         alert('error');
+       })
+       .always(function(){
+         $('.submit-btn').prop('disabled', false);
+        })
+    })
+});
