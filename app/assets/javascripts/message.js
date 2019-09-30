7089db801d367d949f850d67795df25553bfd72a
tmiskw <53873965+tmiskw@users.noreply.github.com>
2019-09-30 15:19:52 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,39 +1,39 @@
-$(document).on('turbolinks:load', function() { 
-     function buildHTML(message){
-      var messageImage = message.image ? `<img class="lower-message__image" src="${message.image}">` : `` ;
-      var html = ` <div class= "message" "data-message-id"=${message.id}>
-                      <div class="upper-message">
-                        <div class="upper-message__user-name">
-                          ${message.user_name}
-                        </div>
-                        <div class="upper-message__date">
-                          ${message.date}
-                        </div>
-                      </div>
-                      <div class="lower-message">
+$(function(){ 
+  function buildHTML(message){
+    var messageImage = message.image ? `<img class="lower-message__image" src="${message.image}">` : `` ;
+    var html = ` <div class= "message" "data-message-id"=${message.id}>
+    <div class="upper-message">
+    <div class="upper-message__user-name">
+    ${message.user_name}
+    </div>
+    <div class="upper-message__date">
+    ${message.date}
+    </div>
+    </div>
+    <div class="lower-message">
                         <div class="lower-message__content">
                           ${message.content}
-                        </div>
+                          </div>
                           ${messageImage}
                        </div> 
-                   </div>`
+                       </div>`
        return html;
     } 
 
     $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
-      var url = $(this).attr('action')
+      var url = $(this).attr('action');
       $.ajax({
         url: url,
         type: "POST",
         data: formData,
         dataType: 'json',
         processData: false,
-        contentType: false
+        contentType: false,
       })
-       .done(function(data){
-         var html = buildHTML(data);
+      .done(function(data){
+        var html = buildHTML(data);
          $('.messages').append(html);      
          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
          $('#new_message')[0].reset();
@@ -43,19 +43,21 @@ $(document).on('turbolinks:load', function() {
        })
        .always(function(){
          $('.form__submit').prop('disabled', false);
-     });
+        });
+      });
 
     var reloadMessages = function () {
       if (window.location.href.match(/\/groups\/\d+\/messages/)){
-        var last_message_id = $('.message:last').data("message-id");
+        var last_message_id = $(".message:last").data("message-id");
   
         $.ajax({
           url: "api/messages",
-          type: 'get',
+          type: 'GET',
           dataType: 'json',
-          data: {last_id: last_message_id}
+          data: {last_id: last_message_id},
         })
         .done(function (messages) {
+          console.log(messages)
           var insertHTML = '';
           messages.forEach(function (message) {
             insertHTML = buildHTML(message);
@@ -70,4 +72,3 @@ $(document).on('turbolinks:load', function() {
     };
     setInterval(reloadMessages, 5000);
   })
-});
