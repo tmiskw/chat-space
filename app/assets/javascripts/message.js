88a789d88f421651e3eddb15a0c4721b090985d2
tmiskw <53873965+tmiskw@users.noreply.github.com>
2019-09-30 20:47:02 +0900

    automatic update function correction

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){ 
   function buildHTML(message){
     var messageImage = message.image ? `<img class="lower-message__image" src="${message.image}">` : `` ;
-    var html = ` <div class= "message" "data-message-id"=${message.id}>
+    var html = ` <div class= "message" data-message-id="${message.id}">
     <div class="upper-message">
     <div class="upper-message__user-name">
     ${message.user_name}
@@ -56,14 +56,16 @@ $(function(){
           dataType: 'json',
           data: {last_id: last_message_id},
         })
-        .done(function (messages) {
-          console.log(messages)
-          var insertHTML = '';
-          messages.forEach(function (message) {
-            insertHTML = buildHTML(message);
+        .done(function (data) {
+          console.log(data)
+          if(data.length > 0){
+            var insertHTML = '';
+            data.forEach(function (message) {
+              insertHTML += buildHTML(message);
+            })
             $('.messages').append(insertHTML);
-          })
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          }
         })
         .fail(function () {
           alert('自動更新に失敗しました');
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,11 @@
 class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @messages = @group.messages.includes(:user).where("id > ?", params[:last_id])
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{@new_message = @messages.where("id > ? and group_id = ?",params[:last_id] ,params[:group_id])}
+    end
   end
 end
